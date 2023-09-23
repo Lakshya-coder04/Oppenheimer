2e54683b033f78d7c82288803eda3124e3092569
Lakshya Birla <71527917+Lakshya-coder04@users.noreply.github.com>
2023-09-23 03:03:24 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ createSparkles();
 
 //close button for the spotify modal
 function closeSpotify() {
-  const closeButton = document.querySelector('.close');
-  const modal = document.querySelector('.modal');
+  const closeButton = document.querySelector('.close-spotify');
+  const modal = document.querySelector('.modal-spotify');
   const spotify = document.getElementById('spotify');
 
   spotify.onclick = function () {
@@ -39,4 +39,107 @@ function closeSpotify() {
 //call the function
 closeSpotify();
 
+
+//close button for the biography modal
+function closeBio(){
+  const closeButton = document.querySelector('.close-bio');
+  const modal = document.querySelector('.modal-bio');
+  const bio = document.getElementById('bio');
+
+  bio.onclick = function () {
+    modal.style.display = 'flex';
+  };
+
+  closeButton.onclick = function () {
+    modal.style.display = 'none';
+  };
+
+  //user click anywhere just close the modal
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+}
+closeBio();
+
 //api key forsquare: fsq3bSewMuMRwCzpIJIwIUbeSYR97aUj8yrSO3RB+9ghEGQ=
+
+//get longitude and latitude 0f the user's location\
+async function getPos() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        resolve([lat, long]);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
+
+
+
+async function placeSearch() {
+  try {
+      const [lat,long] = await getPos();
+
+      const searchParams = new URLSearchParams({
+        query: 'movies',
+        ll: `${lat},${long}`,
+        open_now: 'false',
+        sort: 'DISTANCE'
+      });
+      const results = await fetch(
+        `https://api.foursquare.com/v3/places/search?${searchParams}`,
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: 'fsq3bSewMuMRwCzpIJIwIUbeSYR97aUj8yrSO3RB+9ghEGQ=',
+          }
+        }
+      );
+      const data = await results.json();
+      console.log(data);
+  } catch (err) {
+      console.error(err);
+  }
+}
+
+placeSearch();
+
+//cast info 3fd2be6f0c70a2a598f084ddfb75487c
+
+async function getMovieCast(movieId) {
+  const apiKey = '3fd2be6f0c70a2a598f084ddfb75487c'; 
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    const cast = data.cast;
+
+    // Create an array of objects containing both name and avatar URL
+    const castWithAvatars = cast.map(actor => ({
+      name: actor.name,
+      avatar: `https://image.tmdb.org/t/p/w185${actor.profile_path}`, // Adjust image size as needed
+    }));
+
+    // Now you have an array of objects with name and avatar URL
+    // console.log('Cast with Avatars:', castWithAvatars);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+// Usage: Pass the movie ID as an argument
+getMovieCast(872585); // Replace 123 with the actual movie ID you want to query
